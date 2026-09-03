447bf3d4b2376055f0b4fffa8925006874d470ba
lixiaoming2 <527725681@qq.com>
2026-09-03 18:30:13 +0800

    udpate to 4.24.2

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
         // 1️⃣ binary SDK (name must match .xcframework inside the zip)
         .binaryTarget(
             name: "HyphenateChat",
-            url: "https://download-sdk.oss-cn-beijing.aliyuncs.com/downloads/HyphenateChat4_22_2.zip",
-            checksum: "f0a81e3112670517436f68db53a426ab7224973738d779addf58441e081408d3"
+            url: "https://download-sdk.oss-cn-beijing.aliyuncs.com/downloads/HyphenateChat4_24_2.zip",
+            checksum: "b7119ddf64a44b6e7eac69680814583843372a75adb67ab661dabcd04bfd8d8a"
         ),
 
         // 2️⃣ wrapper target
